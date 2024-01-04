c4a56c98e5c800337f4709a549db9889205a56e5
Akseli Vartiainen <vartiainen.akseli736@gmail.com>
2024-01-04 16:27:08 +0200

    modified configurations

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import { terser } from 'rollup-plugin-terser';
 
 export default defineConfig({
   build: {
     outDir: 'dist',
     minify: 'terser',
+    rollupOptions: {
+      plugins: [terser()],
+    },
   },
 });
+
